f06821a7333e415bd52fd053e90eace8af37a8e9
sadaharu18 <sada.takahashi@gamil.com>
2011-03-07 01:34:57 +0900

    ss

--- index.php
+++ index.php
@@ -5,21 +5,36 @@
 	$dsn  = 'mysql:dbname=school_management;host=localhost';
 	$user = 'app_user';
 	$pass = 'app_user';
-	$student_data = array();
-	try{
-		$dbh = new PDO( $dsn, $user, $pass );
-		$stt = $dbh->query("SET names utf8" );
-		$stt = $dbh->query("SELECT * FROM student" );
-		while( $row = $stt->fetch( PDO::FETCH_ASSOC )){
-			$student_data[] = $row;	
-		}
-	}	
-	catch( PDOException $e ){
-		print( 'Error:'. $e->getMessage());
-		die();
+	$dbh = new PDO( $dsn, $user, $pass );
+	
+	if( $_GET['p'] == 1 ){
+		lists( $dbh );
+	}else{
+		main();
 	}
-	$smarty->assign('student_data', $student_data);
-	$smarty->display('index.tpl');
+	function main(){
+		echo "main";
+	}
+	function lists(){
+		$student_data = array();
+		global $dbh, $smarty;
+		try{
+				$stt = $dbh->query("SET names utf8" );
+				$stt = $dbh->query("SELECT * FROM student" );
+				while( $row = $stt->fetch( PDO::FETCH_ASSOC )){
+						$student_data[] = $row;	
+				}
+		}	
+		catch( PDOException $e ){
+				print( 'Error:'. $e->getMessage());
+				die();
+		}
+		$smarty->assign('student_data', $student_data);
+		$smarty->display('index.tpl');
 
-	$pdo = null;
+		$pdo = null;
+}
+function delete( $id ){
+
+}
 ?>
